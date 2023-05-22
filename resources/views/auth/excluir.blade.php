6972da1187724b0f66bffebd4262add97fab0e77
Ana Júlia <82275552+anaahnb@users.noreply.github.com>
2023-05-21 21:42:28 -0300

    tela de excluir conta

---
+++ resources/views/auth/excluir.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<body class="bg-black">
+    <div class="w-screen h-screen bg-gray-700 flex flex-col items-center justify-center text-gray-100">
+        <header class="w-full max-w-sm">
+            <span class="font-bold text-3xl text-gray-200">
+                Excluir conta
+            </span> 
+        </header>
+
+        <form action="" class="flex flex-col gap-4 items-stretch w-full max-w-sm mt-10">
+
+            <label for="password" class="flex flex-col gap-3 text-gray-100 font-sans">
+                Senha
+                <input placeholder="Digite a sua senha" class="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-600 w-full focus-within:ring-2 ring-cyan-300 placeholder:text-gray-400" type="password">
+            </label>
+
+            <p class="text-sm text-gray-400">Depois que sua conta for encerrada, todos os seus dados ficarão indisponíveis.</p>
+
+            <button class="mt-12 py-3 px-4 bg-green-400 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-emerald-400 focus:ring-2 ring-white">
+                Confirmar
+            </button>
+        </form>
+    </div> 
+</body>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.editar');
+    return view('auth.excluir');
 });
