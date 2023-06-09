a5431c265a4ad8df8b321e7a5aefd2c11468b11e
João <jvictorml06@gmail.com>
2023-06-09 20:51:10 -0300

    inserção de diario pronta

--- app/Http/Controllers/DiarioController.php
+++ app/Http/Controllers/DiarioController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Diario;
 use Illuminate\Http\Request;
 
 class DiarioController extends Controller
 {
-    //
+    public function store(Request $request){
+        // dd($request->user_id);
+        $diario = Diario::create([
+            'objetivo_id' => $request->user_id,
+            'diario_data' => date('Y-m-d'),
+        ]);
+
+        return redirect()->route('home');
+    }
 }
