378938cf4449dfe108671cec4e844ca3c18dea45
João <jvictorml06@gmail.com>
2023-06-11 15:23:12 -0300

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Objetivo;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use PhpParser\Node\Expr\Cast\Object_;
 
 class HomeController extends Controller
 {
@@ -26,7 +28,8 @@ class HomeController extends Controller
     public function index()
     {
         $user = User::where('id', '=', Auth::id())->first();
+        $objetivos = Objetivo::where('user_id', '=', Auth::id())->all();
 
-        return view('users.home', compact('user'));
+        return view('users.home', compact('user', 'objetivos'));
     }
 }
