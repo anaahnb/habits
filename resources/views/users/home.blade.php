8cab6f4fa9902cae0ae8a3184b9e8aace1acf1f2
Ana Júlia <82275552+anaahnb@users.noreply.github.com>
2023-06-19 23:58:22 -0300

    update

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -58,12 +58,39 @@
             <?php
             for ($i=1;$i<=30; $i++) {
                 $datas = date('Y-m-d', strtotime("-{$i} days"));
-                $diario = $diarios->where('diario_data', '=', $datas)->first();
+                $diario = $diarios->where('diario_data', '=', $datas);
+
+                dd($diario->objetivos);
 
                 if($diario) {
-                    print("<div class='w-10 h-10 bg-green-400 rounded-lg'></div>");
+                    print("
+                    <button class='w-10 h-10 bg-green-400 rounded-lg' data-modal-target='modalVisualizarDiario' data-modal-toggle='modalVisualizarDiario'></button>
+                    <div id='modalVisualizarDiario' tabindex='-1' aria-hidden='true' class='fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full'>
+                        <div class='relative w-full max-w-sm max-h-full'>
+                            <div class='relative bg-gray-700 rounded-lg shadow'>
+                                <div class='flex items-start justify-between p-4 rounded-t'>
+                                    <h3 class='text-3xl font-extrabold text-gray-200'>
+                                        {$diario->diario_data}
+                                    </h3>
+                                    <button type='button' class='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white' data-modal-hide='modalVisualizarDiario'>
+                                        <svg aria-hidden='true' class='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd'></path></svg>
+                                        <span class='sr-only'>Fechar modal</span>
+                                    </button>
+                                </div>
+                                
+                                    <div class='p-6 space-y-6'>
+                                        <ul>
+
+                                        </ul>
+                                    
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    ");
                 } else {
-                    print("<div class='w-10 h-10 bg-gray-800 border border-2 border-gray-700 rounded-lg'></div>");
+                    print("<button class='w-10 h-10 bg-gray-800 border border-2 border-gray-700 rounded-lg' data-modal-target='modalVisualizarDiario' data-modal-toggle='modalVisualizarDiario'></button>");
                 }
             }        
             ?>
@@ -72,6 +99,7 @@
 
     <!-- Modal -->
     @include('diarios.inserir')
+    @include('diarios.visualizar')
     @include('objetivos.inserir')
     @include('users.editar')
 
